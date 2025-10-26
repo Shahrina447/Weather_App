5555ce984d4cf3573f0496326faea469a7c93269
Shahrina447 <23018020084@skt.umt.edu.pk>
2025-10-26 22:56:34 +0500

    Initial commit

---
+++ src/app/api/weather/route.js
@@ -0,0 +1,27 @@
+import {  NextResponse } from "next/server";
+
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+   const address = searchParams.get("address");
+  const latitude = searchParams.get("lat");
+
+  const longitude = searchParams.get("lon");
+  let url = "";
+  const API_KEY = "c98a2bead76804db575ecb472e56f1aa"; 
+
+  if (address) {
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${address}&appid=${API_KEY}&units=metric`;
+  } else if (latitude && longitude) {
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  } else {
+    return NextResponse.json({ error: "No location provided" }, { status: 400 });
+  }
+  
+  
+  console.log(url);
+  const res = await fetch(url);
+
+  const data = await res.json();
+  return NextResponse.json({ data });
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import styles from "./app.module.css";
+
+function getCurrentDate() {
+  const currentDate = new Date();
+  const options = { month: "long" };
+  const monthName = currentDate.toLocaleString("en-US", options);
+  const date = new Date().getDate() + " " + monthName;
+  return date;
+}
+
+const Home = () => {
+  const date = getCurrentDate();
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Sialkot");
+
+  async function fetchData(cityName) {
+    try {
+      const response = await fetch(
+        "http://localhost:3000/api/weather?address=" + cityName
+      );
+      const jsonData = (await response.json()).data;
+      setWeatherData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  async function fetchDataByCoordinates(latitude, longitude) {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/weather?lat=${latitude}&lon=${longitude}`
+      );
+      const jsonData = (await response.json()).data;
+      setWeatherData(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+   
+    if (navigator.geolocation) {
+ 
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          fetchDataByCoordinates(lat, lon); 
+        },
+        () => {
+          console.log("Unable to get location ");
+        }
+      );
+    } else {
+      console.log("Geolocation  not supported by this browser ");
+    }
+  }, []);
+  
+
+  return (
+    <main className={styles.main}>
+    <div className={styles.widget}>
+      <form
+        className={styles.weatherLocation}
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchData(city);
+        }}
+      >
+        <input
+          className={styles.input_field}
+          placeholder="Enter city name"
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className={styles.search_button} type="submit">
+          Search
+        </button>
+      </form>
+  
+      {weatherData ? (
+        <>
+          <div className={styles.weatherInfo}>
+            <div className={styles.temperature}>
+              {Math.round(weatherData.main?.temp)}°C
+            </div>
+            <div className={styles.weatherCondition}>
+              {weatherData.weather?.[0]?.description?.toUpperCase()}
+            </div>
+          </div>
+  
+          <div className={styles.place}>{weatherData.name}</div>
+          <div className={styles.date}>{date}</div>
+        </>
+      ) : (
+        <div className={styles.place}>Load HO RAHA HU WAIT KRO......</div>
+      )}
+    </div>
+    <div>
+
+    </div>
+  </main>
+  
+  );
+};
+
+export default Home;
